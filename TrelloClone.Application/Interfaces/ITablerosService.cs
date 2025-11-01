820cf81ff9f320ea9ae6dd5757fbb50055fd9bc0
MateoDelgadoU <mateldraka@gmail.com>
2025-11-01 14:30:18 -0300

    agregué unas cosas al main porque me olvidé de hacer mi rama

---
+++ TrelloClone.Application/Interfaces/ITablerosService.cs
@@ -0,0 +1,14 @@
+﻿using TrelloClone.Application.DTOs.Tableros;
+using TrelloClone.Application.DTOs.Tarjetas;
+
+namespace TrelloClone.Application.Interfaces
+{
+    internal interface ITablerosService
+    {
+        Task<List<TableroDto>> GetAllAsync(string? search = null, string? estado = null);
+        Task<TableroDto?> GetByIdAsync(int id);
+        Task<TableroDto> CreateAsync(CreateTarjetaDto dto);
+        Task<TableroDto?> UpdateAsync(int id, CreateTarjetaDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ TrelloClone.Infraestructure/Services/TablerosService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrelloClone.Infraestructure.Services
+{
+    public class TablerosService
+    {
+    }
+}
